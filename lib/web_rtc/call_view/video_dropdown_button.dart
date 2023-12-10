8bb373b38fc527b90a6d85cb9d1799d0fa3f3c33
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2023-12-10 00:24:40 -0600

    Feat: callback added to CustomDropdownButton

--- lib/web_rtc/call_view/video_dropdown_button.dart
+++ lib/web_rtc/call_view/video_dropdown_button.dart
@@ -3,11 +3,14 @@ import 'package:xapptor_communication/web_rtc/call_view/set_local_renderer.dart'
 import 'package:xapptor_communication/web_rtc/custom_dropdown_button.dart';
 
 extension StateExtension on CallViewState {
-  CustomDropdownButton video_dropdown_button() {
+  CustomDropdownButton video_dropdown_button({
+    required Function(String new_value) callback,
+  }) {
     return CustomDropdownButton(
       value: current_video_device.value,
       on_changed: (new_value) {
         set_local_renderer(new_value!);
+        callback(new_value);
       },
       items: video_devices.value.map((e) => e.label).toList(),
       title: widget.text_list[1],
