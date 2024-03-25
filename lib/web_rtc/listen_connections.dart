e82820c185472e6dbde802462e5677af80fd48bd
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-03-25 10:57:00 -0600

    Fix: extension StateExtension on CallViewState added

--- lib/web_rtc/listen_connections.dart
+++ lib/web_rtc/listen_connections.dart
@@ -8,100 +8,102 @@ import 'package:xapptor_communication/web_rtc/signaling/create_room.dart';
 import 'package:xapptor_communication/web_rtc/signaling/model/connection.dart';
 import 'package:xapptor_communication/web_rtc/signaling/create_connection_anwser.dart';
 
-listen_connections({
-  required Function setState,
-  required Function clean_the_room,
-  required Function({
-    required String message,
-  }) exit_from_room,
-  required BuildContext context,
-}) {
-  bool first_time = true;
-  DocumentReference room_ref = db.collection('rooms').doc(room?.value.id);
-  CollectionReference connections_ref = room_ref.collection("connections");
+extension StateExtension on CallViewState {
+  listen_connections({
+    required Function setState,
+    required Function clean_the_room,
+    required Function({
+      required String message,
+    }) exit_from_room,
+    required BuildContext context,
+  }) {
+    bool first_time = true;
+    DocumentReference room_ref = db.collection('rooms').doc(room?.value.id);
+    CollectionReference connections_ref = room_ref.collection("connections");
 
-  // At first call "snapshots().listen" retrieve all docs in the collection
-  connections_listener.value = connections_ref.snapshots().listen((connections) async {
-    if (!first_time) {
-      if (connections.docs.isEmpty) {
-        if (widget.user_id == room?.value.host_id && ROOM_CREATOR_RANDOM_ID == room?.value.temp_id) {
-          clean_the_room();
+    // At first call "snapshots().listen" retrieve all docs in the collection
+    connections_listener.value = connections_ref.snapshots().listen((connections) async {
+      if (!first_time) {
+        if (connections.docs.isEmpty) {
+          if (widget.user_id == room?.value.host_id && ROOM_CREATOR_RANDOM_ID == room?.value.temp_id) {
+            clean_the_room();
+          } else {
+            exit_from_room(
+              message: "The host closed the room",
+            );
+          }
         } else {
-          exit_from_room(
-            message: "The host closed the room",
-          );
-        }
-      } else {
-        for (var element in connections.docChanges) {
-          //
-          // If a new document is added to the collection
-          if (element.type == DocumentChangeType.added) {
-            debugPrint('Connection_added');
-            Connection connection =
-                Connection.from_snapshot(element.doc.id, element.doc.data() as Map<String, dynamic>);
-
+          for (var element in connections.docChanges) {
             //
-            // Check if the new connection is for me
-            if (widget.user_id == connection.destination_user_id) {
-              debugPrint('new_connection_is_for_me');
+            // If a new document is added to the collection
+            if (element.type == DocumentChangeType.added) {
+              debugPrint('Connection_added');
+              Connection connection =
+                  Connection.from_snapshot(element.doc.id, element.doc.data() as Map<String, dynamic>);
 
-              create_connection_anwser(
-                connection: connection,
-                room_ref: room_ref,
-                setState: setState,
-                callback: () {
-                  _add_remote_renderer(
-                    remote_renderers: remote_renderers,
-                    connection: connection,
-                    user_id: widget.user_id,
-                    setState: setState,
-                  );
-                },
-              );
+              //
+              // Check if the new connection is for me
+              if (widget.user_id == connection.destination_user_id) {
+                debugPrint('new_connection_is_for_me');
+
+                create_connection_anwser(
+                  connection: connection,
+                  room_ref: room_ref,
+                  setState: setState,
+                  callback: () {
+                    _add_remote_renderer(
+                      remote_renderers: remote_renderers,
+                      connection: connection,
+                      user_id: widget.user_id,
+                      setState: setState,
+                    );
+                  },
+                );
+              }
+            }
+            //
+            // If a  document is removed to the collection
+            else if (element.type == DocumentChangeType.removed) {
+              remote_renderers.value.removeWhere((remote_renderer) => remote_renderer.connection_id == element.doc.id);
+              setState(() {});
             }
           }
-          //
-          // If a  document is removed to the collection
-          else if (element.type == DocumentChangeType.removed) {
-            remote_renderers.value.removeWhere((remote_renderer) => remote_renderer.connection_id == element.doc.id);
-            setState(() {});
-          }
         }
+      } else {
+        first_time = false;
       }
-    } else {
-      first_time = false;
+    });
+  }
+
+  _add_remote_renderer({
+    required ValueNotifier<List<RemoteRenderer>> remote_renderers,
+    required Connection connection,
+    required String user_id,
+    required Function setState,
+  }) async {
+    if (remote_renderers.value.isEmpty) {
+      await add_remote_renderer(
+        remote_renderers: remote_renderers,
+        stream: null,
+        setState: setState,
+      );
     }
-  });
-}
+    remote_renderers.value.last.connection_id = connection.id;
+    String user_id = "";
 
-_add_remote_renderer({
-  required ValueNotifier<List<RemoteRenderer>> remote_renderers,
-  required Connection connection,
-  required String user_id,
-  required Function setState,
-}) async {
-  if (remote_renderers.value.isEmpty) {
-    await add_remote_renderer(
-      remote_renderers: remote_renderers,
-      stream: null,
-      setState: setState,
+    if (connection.source_user_id != user_id) {
+      user_id = connection.source_user_id;
+    } else if (connection.destination_user_id != user_id) {
+      user_id = connection.destination_user_id;
+    }
+
+    remote_renderers.value.last.user_id = user_id;
+    User user = await get_user_from_id(
+      user_id,
     );
+    remote_renderers.value.last.user_name = user.name;
+
+    setState(() {});
+    debugPrint("connection_id: ${connection.id}");
   }
-  remote_renderers.value.last.connection_id = connection.id;
-  String user_id = "";
-
-  if (connection.source_user_id != user_id) {
-    user_id = connection.source_user_id;
-  } else if (connection.destination_user_id != user_id) {
-    user_id = connection.destination_user_id;
-  }
-
-  remote_renderers.value.last.user_id = user_id;
-  User user = await get_user_from_id(
-    user_id,
-  );
-  remote_renderers.value.last.user_name = user.name;
-
-  setState(() {});
-  debugPrint("connection_id: ${connection.id}");
 }
